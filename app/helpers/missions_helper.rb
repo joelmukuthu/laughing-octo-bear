c2e85bb062946d48ff5d1464b0c9a1c2a7729351
Joel Mukuthu <joelmukuthu@gmail.com>
2014-06-11 00:52:14 +0300

    limit who can see mission owner's name

--- app/helpers/missions_helper.rb
+++ app/helpers/missions_helper.rb
@@ -2,4 +2,11 @@ module MissionsHelper
   def can_update_mission? mission
     current_user == mission.owner
   end
+
+  def can_see_mission_owner_name? mission
+    return true if current_user == mission.owner
+    return true if mission.who_can_see_my_name == Mission::ANYONE_CAN_SEE_MY_NAME
+    return true if mission.sponsored_by? current_user
+    # TODO: should we also let invitees who haven't accepted their invitations to see the name too?
+  end
 end
